e0c245a4aaf1aafff6d458d2c030d439195070ca
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2025-08-22 23:07:08 +0530

    marketplacepage.jsx file completed

--- Kala/frontend/src/pages/MarketplacePage.jsx
+++ Kala/frontend/src/pages/MarketplacePage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from '../components/Loader';
+import ArtCard from '../components/ArtCard';
 
 const MarketplacePage = () => {
+
+    const[artworks,setArtworks] = useState([]);
+    const[loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        const fetchArtworks = async ()=>{
+            try{
+                setLoading(true);
+                const{data} = await axios.get('/api/artworks');
+                setArtworks(data);
+                setLoading(false);
+            }catch(error){
+                console.error("Error fetching artworks:",error);
+                setLoading(false);
+            }
+        };
+        fetchArtworks();
+    },[]);
+
+
   return (
-    <div>MarketplacePage</div>
-  )
+    <div className="container mx-auto px-6 py-12">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-2">
+        Marketplace
+      </h1>
+      <p className='text-center text-gray-600 mb-10'>Browse our curated collection of traditional Indian folk art.</p>
+      {loading?<Loader/>:(
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+            {
+                artworks.map(art=>(
+                    <ArtCard key={art._id} artwork={art}/>
+                ))
+            }
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default MarketplacePage
